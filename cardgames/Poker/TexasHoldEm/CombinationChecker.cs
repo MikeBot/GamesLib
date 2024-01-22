21cdccda2a9adcf24f0d2bab32815ba2d9737ac1
mikegoethals <mr.mikegoethals@gmail.com>
2024-01-23 00:58:57 +0100

    ADD: Added the CombinationChecker class to TexasHoldEm, with unimplemented methods for checking for specific texas holdem combination.

---
+++ cardgames/Poker/TexasHoldEm/CombinationChecker.cs
@@ -0,0 +1,66 @@
+namespace TexasHoldEm;
+
+using Standard52CardDeck;
+
+public class CombinationChecker
+{
+    private static Card[] CheckForHighCard(Card[] holeCards, Card[] communityCards)
+    {
+        // TODO: Implement
+        return [];
+    }
+
+    private static Card[] CheckForPair(Card[] cards)
+    {
+        // TODO: Implement
+        return [];
+    }
+
+    private static Card[] CheckForTwoPairs(Card[] cards)
+    {
+        // TODO: Implement
+        return [];
+    }
+
+    private static Card[] CheckForThreeOfAKind(Card[] holeCards, Card[] communityCards)
+    {
+        // TODO: Implement
+        return [];
+    }
+
+    private static Card[] CheckForFourOfAKind(Card[] holeCards, Card[] communityCards)
+    {
+        // TODO: Implement
+        return [];
+    }
+
+    private static Card[] CheckForFlush(Card[] holeCards, Card[] communityCards)
+    {
+        // TODO: Implement
+        return [];
+    }
+
+    private static Card[] CheckForFullHouse(Card[] holeCards, Card[] communityCards)
+    {
+        // TODO: Implement
+        return [];
+    }
+
+    private static Card[] CheckForStraight(Card[] holeCards, Card[] communityCards)
+    {
+        // TODO: Implement
+        return [];
+    }
+
+    private static Card[] CheckForStraightFlush(Card[] holeCards, Card[] communityCards)
+    {
+        // TODO: Implement
+        return [];
+    }
+
+    private static Card[] CheckForRoyalFlush(Card[] holeCards, Card[] communityCards)
+    {
+        // TODO: Implement
+        return [];
+    }
+}

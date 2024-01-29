ffc0de946aff9e189dd358b67a46148d8c9abfce
mikegoethals <mr.mikegoethals@gmail.com>
2024-01-29 23:57:00 +0100

    ADD: Added tests to DeckTests for dealing and discarding multiple cards at once.

--- cardgames/shared/Standard52CardDeckTest/DeckTests.cs
+++ cardgames/shared/Standard52CardDeckTest/DeckTests.cs
@@ -2,7 +2,7 @@ namespace Standard52CardDeckTest;
 
 using Xunit;
 using Standard52CardDeck;
-
+using System.Numerics;
 
 public class DeckTests
 {
@@ -49,7 +49,7 @@ public class DeckTests
         Assert.Contains(card, deck.DiscardPile);
     }
 
-        [Fact]
+    [Fact]
     public void DiscardFromDeck_Should_Add_The_Specified_Number_Of_Card_To_Discard_Pile()
     {
         // Arrange
@@ -61,5 +61,35 @@ public class DeckTests
         // Assert
         Assert.Equal(3, deck.DiscardPile.Count);
         Assert.Equal(Deck.CARDS_PER_DECK - 3, deck.Cards.Count);
+    }
+
+    [Fact]
+    public void Deal_Multiple_Cards_Should_Return_Correct_Number_Of_Cards()
+    {
+        //Arrange
+        var deck = new Deck();
+        Random random = new Random();
+
+        //Act
+        int numberOfCardsDealed = random.Next(1, 52);
+        Card[] cardsDealed = deck.DealMultiple(numberOfCardsDealed, "Player 1");
+
+        //Assert
+        Assert.Equal(numberOfCardsDealed, cardsDealed.Length);
+    }
+
+    [Fact]
+    public void Deal_Multiple_Cards_Should_ADD_CARDS_TO_DEALED_CARDS()
+    {
+        //Arrange
+        var deck = new Deck();
+        Random random = new Random();
+
+        //Act
+        int numberOfCardsDealed = random.Next(1, 52);
+        deck.DealMultiple(numberOfCardsDealed, "Player 1");
+
+        //Assert
+        Assert.Equal(numberOfCardsDealed, deck.DealedCards.Count);
     }
 }

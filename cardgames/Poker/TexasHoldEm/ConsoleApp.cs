49bfc8c579de142f9f3daa12cf343592d7084cec
mikegoethals <mr.mikegoethals@gmail.com>
2024-01-30 22:23:46 +0100

    ADD: Created a console app for the texas hold en game. However, this is still unimplemented.

---
+++ cardgames/Poker/TexasHoldEm/ConsoleApp.cs
@@ -0,0 +1,20 @@
+namespace TexasHoldEm;
+
+public class ConsoleApp {
+
+    public static void Main(string[] args) {
+        Boolean endGame = false;
+        GameEngine gameEngine = new GameEngine();
+
+        while(!endGame) {
+
+            
+            
+            endGame = true;
+        }
+    }
+
+    private void printGameStartUp() {
+        //TODO: Print a Game Start Up Message
+    }
+}
